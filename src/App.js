56873e45df00947f8b7658bf609d049789791935
Ely Karr <elykarr@gmail.com>
2022-01-08 13:36:51 -0800

    welcome page and more style.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import Search from "./components/Search"
 import NavBar from "./components/NavBar"
 import ArtworksContainer from "./components/ArtworksContainer";
 import ArtworkDetail from "./components/ArtworkDetail";
+import Welcome from "./components/Welcome"
 import "./styles.css";
+
 
 
 function App() {
@@ -19,16 +21,6 @@ function App() {
   //   const text = "Rijkspo"
   //   document.title = text;
   // }, [text]);
-
-
-
-  // console.log(process.env.REACT_APP_API_KEY)
-
-  // fetch(`https://www.rijksmuseum.nl/api/nl/collection?key=${process.env.REACT_APP_API_KEY}&search?p=1&ps=12&who=Rembrandt&st=Objects&ii=6`,{
-  //   .then(resp => resp.json())
-  //   .then(data => console.log(data))
-  // })
-
 
   return (
     <div>
@@ -54,7 +46,7 @@ function App() {
             <ArtworksContainer searchResults={searchResults} />
           </Route>
           <Route exact path="/">
-            <div>Home</div>
+            <Welcome />
           </Route>
         </Switch>
       </div>
@@ -63,45 +55,3 @@ function App() {
 }
 
 export default App;
-
-
-// All works on display in the museum currently and with an image
-// https://www.rijksmuseum.nl/en/search?p=3&ps=12&ondisplay=True&st=Objects&ii=0
-
-// All works on display in the museum with an image and search string Rembrandt
-// https://www.rijksmuseum.nl/en/search?q=Rembrandt&v=&s=&ondisplay=True&ii=0&p=1
-
-// search: Rembrandt
-// https://www.rijksmuseum.nl/en/search?p=1&ps=12&who=Rembrandt&st=Objects
-
-// search: rembrandt
-// https://www.rijksmuseum.nl/en/search?p=1&ps=12&who=Rembrandt&st=Objects
-
-// search: painting and in the museum
-// https://www.rijksmuseum.nl/en/search?q=painting&v=&s=&ondisplay=True&ii=0&p=1
-
-// fetch all of them
-// so when the user clicks, another fetch request
-// modal pop up,
-// save the results in state
-// p=1&ps=100
-// 1 page, 100 results
-
-
-// State can be declared in paintingsContainer, and withing paintingsContainer, filers results saved in state
-// in that filtered variable
-// on Search
-
-
-// passes down callback function, will pass back up the search term
-
-
-
-// Copy info into a local db.json
-// a form to upload a new artwork POST
-// Visitor log
-
-
-// stretch goal:
-// PATCH to update favorite
-// search
---
+++ src/components/Welcome.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Welcome() {
+    return (
+        <div>
+            <h1>Welcome to Rijkspo, a website for exploring the vast and timeless collection of the Rijksmuseum.</h1>
+            <div className="welcomeImage">
+                <img src="https://rijks-frontend.azureedge.net/assets/d28f8098-0892-43c2-bb26-3fbd5f67ac31?w=1200&h=630&fx=1490&fy=5723&c=6be19a297c4916ad3b4e6c9a91411e4bcc8d13306947b7ac4aa401063e0d136c" />
+            </div>
+        </div>
+    )
+}
+
+export default Welcome;
